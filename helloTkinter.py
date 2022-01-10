452a5d6e6eb28695facd3df1c5d5ef90f3250166
Jeroen-DH <99068895@mydavinci.nl>
2022-01-10 13:35:45 +0100

    Hello tkinter window af

---
+++ helloTkinter.py
@@ -0,0 +1,24 @@
+import tkinter
+from tkinter import ttk
+gui = tkinter.Tk()
+gui.title("Hello!")
+gui.config(bg="Blue")
+gui.geometry("250x200")
+
+textbox = tkinter.Label(
+    gui,
+    text="Hello tkinter!",
+    bg="red",
+    fg="black",
+    width=50,
+    font=100
+    
+)
+textbox.pack(
+    fill= "both",
+    padx=30,
+    pady=40,
+    expand=True
+)
+
+gui.mainloop()
